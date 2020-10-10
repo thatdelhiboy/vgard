920d669f439662a740330b2dc6589abb48d2bb57
Abhishek Singh <47791443+thatdelhiboy@users.noreply.github.com>
2020-10-10 14:27:55 +0530

    style cnaged to php

--- index.php
+++ index.php
@@ -25,9 +25,11 @@
 <!-- test  -->
 <style>
 <?php include "./ui/client/css/style_new.php";?>
+<?php include "./ui/client/css/home_banner.php";?>
+
+
 </style>
-  <!-- <link rel="stylesheet" href="/ui/client/css/style-new.css"> -->
-  <link rel="stylesheet" href="/ui/client/css/home-banner.css">
+
   <!--[if lt IE 10]>
         <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="httpqwertywindows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
         <script src="https://www.vguard.in/ui/client/scripts/html5shiv.min.js"></script>
