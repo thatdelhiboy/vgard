2e224f1d63cea2563593458dac58ac073ef6719b
Abhishek Singh <47791443+thatdelhiboy@users.noreply.github.com>
2020-11-21 10:50:38 +0530

    Update brochure.php

--- brochure.php
+++ brochure.php
@@ -2,7 +2,7 @@
 
 <section class="section-50 section-sm-90 brandPhilosophySection" id="aboutus">
       <div class="shell text-center">
-        <h1 class="orange" >About Us</h1>
+        <h1 class="orange" >Brochure</h1>
         <div class="range range-xs-center offset-top-40">
           <div class="cell-xs-10 cell-sm-12 cell-md-12 height-fill">
           <embed src="https://www.rehaaninternational.com/Livpower%20Voltage%20Brochure.pdf" width="100%" height="600px" 
