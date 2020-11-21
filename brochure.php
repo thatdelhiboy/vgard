49bf8d594bce801d3d490d3e64213a39d408a1df
Abhishek Singh <47791443+thatdelhiboy@users.noreply.github.com>
2020-11-21 11:11:55 +0530

    commit

--- brochure.php
+++ brochure.php
@@ -5,7 +5,8 @@
         <h1 class="orange" >Brochure</h1>
         <div class="range range-xs-center offset-top-40">
           <div class="cell-xs-10 cell-sm-12 cell-md-12 height-fill">
-          <embed src="https://www.rehaaninternational.com/Livpower%20Voltage%20Brochure.pdf" width="100%" height="600px" 
+          <embed src="https://drive.google.com/viewerng/
+viewer?embedded=true&url=https://www.rehaaninternational.com/Livpower%20Voltage%20Brochure.pdf" width="100%" height="600px" 
  type="application/pdf">
           </div>
         </div>
